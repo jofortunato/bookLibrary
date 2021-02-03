cc355fd416cf503a6250b7835b127e93081a8fbb
Joao Fortunato <joaomlfortunato@gmail.com>
2021-02-03 12:48:40 +0000

    Add Progress Status to the Book Object

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
 let myLibrary = [];
 
-function Book(title, author, pagesTotal, pagesRead) {
+function Book(title, author, pagesTotal, pagesRead, progressStatus) {
   this.title = title;
   this.author = author;
   this.pagesTotal = pagesTotal;
   this.pagesRead = pagesRead;
+  this.progressStatus = progressStatus;
 }
 
-function addBookToLibrary(title, author, pagesTotal, pagesRead) {
-  newBook = new Book(title, author, pagesTotal, pagesRead);
+function addBookToLibrary(title, author, pagesTotal, pagesRead, progressStatus) {
+  newBook = new Book(title, author, pagesTotal, pagesRead, progressStatus);
 
   myLibrary.push(newBook);
 }
